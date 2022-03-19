8744f837b8554051bbbb9cebc23a3937be6e92b5
Ruths2 <ruth09@yahoo.com>
2022-03-19 17:43:01 -0300

    Realiza teste na URL=/api/regioes. Teste getControllerTest(/regioes): ok. Teste getByIdControllerTest(/regioes/{id}): ok

--- apiProjetPhilips/backend/phildevweekback/src/test/java/com/ruth_dio/phildevweekback/PhildevweekApplicationTests.java
+++ apiProjetPhilips/backend/phildevweekback/src/test/java/com/ruth_dio/phildevweekback/PhildevweekApplicationTests.java
@@ -1,13 +1,42 @@
 package com.ruth_dio.phildevweekback;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ruth_dio.phildevweekback.controller.ControllerRegiao;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class PhildevweekbackApplicationTests{
+	private final String URL="/api/regioes";
+
+	@Autowired
+	MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@Autowired
+	private ControllerRegiao controllerRegiao;
+	
+	@Test
+	public void getControllerTest() throws Exception {
+		mockMvc.perform(get(URL)).andExpect(status().isOk());
+
+	}
 
 	@Test
-	void contextLoads() {
+	public void getByIdControllerTest() throws Exception {
+		mockMvc.perform(get(URL+"/1")).andExpect(status().isOk());
+
 	}
 
 }
